7add768312b8335050711c4c9acdf04165122bf2
szezso <szezso@szezso-kitchen.(none)>
2014-06-28 12:53:10 +0200

    media-legacy: Add support for qsd8k

--- Android.mk
+++ Android.mk
@@ -1,12 +1,16 @@
 ifeq ($(TARGET_QCOM_MEDIA_VARIANT),legacy)
 QCOM_MEDIA_ROOT := $(call my-dir)
-ifneq ($(filter msm8974 msm8960 msm8660 msm7x27a msm7x27 msm7x30,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8974 msm8960 msm8660 msm7x27a msm7x27 msm7x30 qsdk84,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
 
-ifneq ($(filter msm8974 msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8974 msm8960 msm8660 msm7x30 qsd8k,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
 include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
+endif
+
+ifneq ($(filter qsd8k,$(TARGET_BOARD_PLATFORM)),)
+include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 endif
 endif
