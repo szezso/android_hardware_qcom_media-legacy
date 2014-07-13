066a51a096f8f7212e79d594fb7829c0117ed6ce
Szeleczky Zsombor <szezsoo@gmail.com>
2014-07-13 11:38:19 +0200

    Media-legacy: I was wrong, qsd8k uses prebuilt OMX

--- Android.mk
+++ Android.mk
@@ -1,11 +1,11 @@
 ifeq ($(TARGET_QCOM_MEDIA_VARIANT),legacy)
 QCOM_MEDIA_ROOT := $(call my-dir)
-ifneq ($(filter msm8974 msm8960 msm8660 msm7x27a msm7x27 msm7x30 qsdk84,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8974 msm8960 msm8660 msm7x27a msm7x27 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
 
-ifneq ($(filter msm8974 msm8960 msm8660 msm7x30 qsd8k,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8974 msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
 include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
 endif
